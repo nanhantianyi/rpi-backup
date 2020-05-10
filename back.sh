2ecb03bf435daa2f274aa02dddb3c495bfbdb3c9
kevin <nanhantianyi@126.com>
2020-05-10 21:00:07 +0800

    完善脚本

--- back.sh
+++ back.sh
@@ -1,48 +1,62 @@
 #!/bin/bash
-sudo apt install -y dosfstools parted kpartx rsync
+
+if [ `whoami` != "root" ];then
+    echo "This script must be run as root!"
+    exit 1
+fi
+
+# install software
+apt install -y dosfstools parted kpartx rsync
 
 echo ""
 echo "software is ready"
 
-file="rpi.img"
+file="rpi-`date +%Y%m%d%H%M%S`.img"
 
 if [ "x$1" != "x" ];then
     file="$1"
 fi
 
-dr=`df -P | grep /dev/root | awk '{print $3}'`
+# boot mount point
+boot_mnt=`findmnt -n /dev/mmcblk0p1 | awk '{print $1}'`
+
+root_info=`df -PT / | tail -n 1`
+
+root_type=`echo $root_info | awk '{print $2}'`
+
+dr=`echo $root_info | awk '{print $4}'`
 db=`df -P | grep /dev/mmcblk0p1 | awk '{print $2}'`
 ds=`echo $dr $db |awk '{print int(($1+$2)*1.2)}'`
 
 echo "create $file ..."
 
-sudo dd if=/dev/zero of=$file bs=1K count=0 seek=$ds
-#sudo truncate -s ${ds}k $file
+dd if=/dev/zero of=$file bs=1K count=0 seek=$ds
+#truncate -s ${ds}k $file
 
-start=`sudo fdisk -l /dev/mmcblk0| awk 'NR==9 {print $2}'`
-end=`sudo fdisk -l /dev/mmcblk0| awk 'NR==9 {print $3}'`
+start=`fdisk -l /dev/mmcblk0| awk 'NR==9 {print $2}'`
+end=`fdisk -l /dev/mmcblk0| awk 'NR==9 {print $3}'`
 
 if [ "$start" == "*" ];then
-    start=`sudo fdisk -l /dev/mmcblk0| awk 'NR==9 {print $3}'`
-    end=`sudo fdisk -l /dev/mmcblk0| awk 'NR==9 {print $4}'`
+    start=`fdisk -l /dev/mmcblk0| awk 'NR==9 {print $3}'`
+    end=`fdisk -l /dev/mmcblk0| awk 'NR==9 {print $4}'`
 fi
 
 start=`echo $start's'`
 end=`echo $end's'`
 
-end2=`sudo fdisk -l /dev/mmcblk0| awk 'NR==10 {print $2}'`
+end2=`fdisk -l /dev/mmcblk0| awk 'NR==10 {print $2}'`
 end2=`echo $end2's'`
 
 echo "start=$start"
 echo "end=$end"
 echo "end2=$end2"
 
-sudo parted $file --script -- mklabel msdos
-sudo parted $file --script -- mkpart primary fat32 $start $end
-sudo parted $file --script -- mkpart primary ext4 $end2 -1
+parted $file --script -- mklabel msdos
+parted $file --script -- mkpart primary fat32 $start $end
+parted $file --script -- mkpart primary ext4 $end2 -1
 
-loopdevice=`sudo losetup -f --show $file`
-device=`sudo kpartx -va $loopdevice | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
+loopdevice=`losetup -f --show $file`
+device=`kpartx -va $loopdevice | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
 device="/dev/mapper/${device}"
 
 echo "device=$device"
@@ -61,23 +75,26 @@ opartuuidr=`blkid -o export /dev/mmcblk0p2 | grep PARTUUID`
 npartuuidb=`blkid -o export ${partBoot} | grep PARTUUID`
 npartuuidr=`blkid -o export ${partRoot} | grep PARTUUID`
 
+boot_label=`dosfslabel /dev/mmcblk0p1 | tail -n 1`
+root_label=`e2label /dev/mmcblk0p2 | tail -n 1`
 
-sudo mkfs.vfat -F 32 -n "boot" $partBoot
+mkfs.vfat -F 32 -n "$boot_label" $partBoot
 echo "$partBoot format success"
 
-sudo mkfs.ext4 $partRoot
+mkfs.ext4 $partRoot
+e2label $partRoot $root_label
 echo "$partRoot format success"
 
-sudo mount -t vfat $partBoot /mnt
-sudo cp -rfp /boot/* /mnt/
+mount -t vfat $partBoot /mnt
+cp -rfp ${boot_mnt}/* /mnt/
 
-sudo sed -i "s/$opartuuidr/$npartuuidr/g" /mnt/cmdline.txt
+sed -i "s/$opartuuidr/$npartuuidr/g" /mnt/cmdline.txt
 
 sync
 
-sudo umount /mnt
+umount /mnt
 
-sudo mount -t ext4 $partRoot /mnt
+mount -t ext4 $partRoot /mnt
 
 if [ -f /etc/dphys-swapfile ]; then
     SWAPFILE=`cat /etc/dphys-swapfile | grep ^CONF_SWAPFILE | cut -f 2 -d=`
@@ -89,42 +106,51 @@ fi
 
 cd /mnt
 
-sudo rsync --force -rltWDEgop --delete --stats --progress \
+rsync --force -rltWDEgop --delete --stats --progress \
     $EXCLUDE_SWAPFILE \
     --exclude ".gvfs" \
-    --exclude "/boot" \
+    --exclude "$boot_mnt" \
     --exclude "/dev" \
     --exclude "/media" \
     --exclude "/mnt" \
     --exclude "/proc" \
     --exclude "/run" \
+    --exclude "/snap" \
     --exclude "/sys" \
     --exclude "/tmp" \
     --exclude "lost\+found" \
     --exclude "$file" \
     / ./
 
+if [ ! -d $boot_mnt ]; then
+    mkdir $boot_mnt
+fi
+
+if [ -d /snap ]; then
+    mkdir /mnt/snap
+fi
+
 for i in boot dev media mnt proc run sys boot; do
     if [ ! -d /mnt/$i ]; then
-        sudo mkdir /mnt/$i
+        mkdir /mnt/$i
     fi
 done
 
 if [ ! -d /mnt/tmp ]; then
-    sudo mkdir /mnt/tmp
-    sudo chmod a+w /mnt/tmp
+    mkdir /mnt/tmp
+    chmod a+w /mnt/tmp
 fi
 
 cd
 
-sudo sed -i "s/$opartuuidb/$npartuuidb/g" /mnt/etc/fstab
-sudo sed -i "s/$opartuuidr/$npartuuidr/g" /mnt/etc/fstab
+sed -i "s/$opartuuidb/$npartuuidb/g" /mnt/etc/fstab
+sed -i "s/$opartuuidr/$npartuuidr/g" /mnt/etc/fstab
 
 sync
 
-sudo umount /mnt
+umount /mnt
 
-sudo kpartx -d $loopdevice
-sudo losetup -d $loopdevice
+kpartx -d $loopdevice
+losetup -d $loopdevice
 
 
